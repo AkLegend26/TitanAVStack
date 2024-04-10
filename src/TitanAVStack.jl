26da51e8a5d951ed20e08ff1137fb5323b03e103
Ryan McCord <ryanmccord@Ryans-MacBook-Pro-94.local>
2024-04-10 10:36:37 -0500

    routing and control (not working)

--- src/TitanAVStack.jl
+++ src/TitanAVStack.jl
@@ -3,7 +3,12 @@ module TitanAVStack
 using VehicleSim
 using Sockets
 using Serialization
+using LinearAlgebra
+using StaticArrays
 
+include("Routing.jl")
+include("control.jl")
+include("decision_making.jl")
 include("client.jl")
 include("example_project.jl")
 
